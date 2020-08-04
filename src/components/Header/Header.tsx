812f2f7fd22fec1ba8d439ef6711c7c7ef928151
martos-art <matros.artm@gmail.com>
2020-08-04 18:21:59 +0300

    add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import hdr from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={hdr.header}>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png'/>
+        </header>
+    )
+    };
+
+    export default Header;
